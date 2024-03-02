f63329496311e4c67cc5dd084fb00de837df52c4
Luis Gil <luisfrancisco17msn@gmail.com>
2024-03-02 10:01:45 -0500

    Lista y validación de tareas repetidas

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,37 @@ import { TaskCreator } from "./components/TaskCreator";
 
 const App = () => {
 
+  const [tasksItems, setTasksItems] = useState([
+    {name : 'primera tarea', done: false},
+    {name : 'segunda tarea', done: false},
+    {name : 'tercera tarea', done: false}
+  ])
+
+  function createNewTask(val) {
+    if(!tasksItems.find(task=> task.name ===  val)){
+      setTasksItems([...tasksItems, {name: val, done: 'false'}])
+    }
+  }
+
   return (
     <div className="App">
-      <TaskCreator />
+      <TaskCreator createNewTask={createNewTask}/>
+      <table>
+        <thead>
+          <tr>
+            <th>Tasks</th>
+          </tr>
+        </thead>
+        <tbody>
+            {
+            tasksItems.map( item =>(
+              <tr key={item.name}>
+                <td> {item.name} </td>
+              </tr> ))
+            }
+        </tbody>
+      </table>
+      
     </div>
   );
 };
